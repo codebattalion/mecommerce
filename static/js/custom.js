d75d0d37973301a5f939c66162849958453130c0
codebattalion <vipanpreet.code@gmail.com>
2020-09-30 12:18:40 -0700

    Navbar 60% completed

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,32 +1,48 @@
+// $(window).scroll(function () {
+//   if ($(this).scrollTop() < 30) {
+//       $(".nav").css({
+//           'background': 'transparent',
+//           'height': '100px',
+//           'color': '#333',
+//           'boxShadow': '0px 0px 0px 0px rgba(0,0,0,0.1)'
 
-  $(window).scroll(function () { 
-    if ($(this).scrollTop() < 30) {
-        $(".nav").css({
-            'background': 'transparent',
-            'height': '100px',
-            'color': '#333',
-            'boxShadow': '0px 0px 0px 0px rgba(0,0,0,0.1)'
+//       });
 
-        });
-     
-    }
-    else{
-        $(".nav").css({
-            'background': '#fff',
-            'height': '80px',
-            'color': '#333',
-            'boxShadow': '0px 1px 10px 2px rgba(0,0,0,0.1)'
-        });
-      
-    }
-            
-  });
+//   }
+//   else{
+//       $(".nav").css({
+//           'background': '#fff',
+//           'height': '80px',
+//           'color': '#333',
+//           'boxShadow': '0px 1px 10px 2px rgba(0,0,0,0.1)'
+//       });
 
-  $('.cart-trigger').click(function(){
-    $('.cart').toggleClass("cart-open");
-   
-  });
-  $('.nav--trigger').click(function(){
-    $('.nav__bar').toggleClass("nav--open");
-    $('.header').toggleClass("header--open");
-  });
+//   }
+
+// });
+
+$(".cart-trigger").hover(function () {
+  $(".cart").toggleClass("cart-open");
+});
+$(".cart").hover(function () {
+  $(this).toggleClass("cart-open");
+});
+
+$(".search--trigger").hover(function () {
+  $(".search").toggleClass("search-open");
+});
+$(".search").hover(function () {
+  $(this).toggleClass("search-open");
+});
+
+$(".usercard--trigger").hover(function () {
+  $(".usercard").toggleClass("usercard-open");
+});
+$(".usercard").hover(function () {
+  $(this).toggleClass("usercard-open");
+});
+
+$(".nav--trigger").click(function () {
+  $(".nav__bar").toggleClass("nav--open");
+  $(".header2").toggleClass("header--open");
+});
